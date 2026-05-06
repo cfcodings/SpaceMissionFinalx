e2aa6c8445a8608327904fb04bc0c1d18910f511
cfcodings <cfrancois14@outlook.com>
2026-05-05 20:43:07 -0400

    Initializing some classes, this is the basic stuff yk

--- src/Crew.java
+++ src/Crew.java
@@ -1,2 +1,13 @@
 public class Crew {
+    // Crew Info
+    String name;
+    String role;
+    int health = 100;
+    boolean alive = true;
+
+    // Constructor to set up crew member
+    public Crew(String chosenName, String crewRole) {
+        name = chosenName;
+        role = crewRole;
+    }
 }
--- src/Mission.java
+++ src/Mission.java
@@ -2,7 +2,11 @@
 // SID: 1371909
 
 public class Mission {
+    int missionDay = 1; // Current day of the mission
+    int distance = 0; // Distance travelled for the mission
+
     static void main(String[] args) {
+        boolean running = true;
 
     }
 }
