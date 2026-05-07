64319e36db8aab31b1a2b42610450f8ded15b04d
cfcodings <cfrancois14@outlook.com>
2026-05-06 20:44:12 -0400

    Basically finished the project, but there might be minor bugs I couldn't catch
    Can't really do much more because of the time crunch, so there's that
    
    If there's spare time I can add some bonus features but if not then it is what it is

--- src/Mission.java
+++ src/Mission.java
@@ -10,6 +10,7 @@ public class Mission {
 
     static int missionDay = 1; // Current day of the mission
     static int distance = 0; // Distance traveled for the mission
+    static String event = "Clear"; // Current event for the day
 
     static Crew[] crewMembers = new Crew[3]; // 3 crew members per mission
     static Ship flyingShip = new Ship(); // The ship the crew will fly on
@@ -17,27 +18,46 @@ public class Mission {
 
     static final String[] ROLES = {"Pilot", "Co-Pilot", "Crewmate"}; // Crew roles
     static final String[] SHIP_CONDITIONS = {"Maintained", "Blemished", "Impaired", "Critical"}; // Ship conditions
+    static final String[] EVENTS = {"Meteor Shower", "Solar Flare", "Supply Cache"};
     static final int DISTANCE_REQUIRED = 100; // Distance required to win
 
     // Show the instructions on what to do
     public static void showInstructions() {
-        System.out.println("\nWelcome to Space Mission Control! Manage your crew and limited resources to reach Mars!");
+        System.out.println("Welcome to Space Mission Control! Manage your crew and limited resources to reach Mars!");
         System.out.println("You begin with 100 fuel, oxygen and supplies, which are used to manage the ship and crew.");
         System.out.println("Every action has a cost, whether it be from the crew or the ship, and daily events also pose a risk.");
         System.out.println("Your crew is at peak health, can you keep it that way?");
     }
 
     public static void displayShip() {
+        // Use ship info for multiple status strings
         System.out.println("\nShip Status: ");
         System.out.println("Oxygen: " + flyingShip.oxygen);
         System.out.println("Fuel: " + flyingShip.fuel);
         System.out.println("Supplies: " + flyingShip.supplies);
-        System.out.println("Condition: " + flyingShip.condition);
+        System.out.println("Condition: " + SHIP_CONDITIONS[flyingShip.condition]);
+
+        // Use mission info for information till completion
+        System.out.println("\nDistance to Mars: " + distance + "/" + DISTANCE_REQUIRED + " Mm");
+        System.out.println("Mission Day " + missionDay);
+
+        // Display event information
+        if (event.equals(EVENTS[0])) {
+            // Meteor shower
+            System.out.println("A METEOR SHOWER has damaged your ship today!");
+        } else if (event.equals(EVENTS[1])) {
+            // Solar Flare
+            System.out.println("A SOLAR FLARE has harmed your crewmates today!");
+        } else if (event.equals(EVENTS[2])) {
+            // Supply Cache
+            System.out.println("A hidden SUPPLY CACHE has given you extra supplies today!");
+        }
     }
 
     public static void displayCrew() {
         System.out.println("\nCrew Status: ");
 
+        // Use crew info to make a status string
         for (Crew member : crewMembers) {
             System.out.println(member.name + " / " + member.role + " / " + member.health + " Health / " + member.getStatus() + " / " + member.getAction());
         }
@@ -60,18 +80,63 @@ public class Mission {
         }
 
         while (running) {
+            // Line break for start of action tree
+            System.out.println("-----------------------------------------------");
+
             // Print instructions on initialization
             if (!initialized) {
                 initialized = true;
                 showInstructions();
                 resourceHistory.append("DAY 1:");
             } else {
+                if (distance >= DISTANCE_REQUIRED) {
+                    // You won due to distance! Nice
+                    int crewAlive = 0;
+
+                    for (Crew member : crewMembers) {
+                        if (member.alive) {
+                            crewAlive += 1;
+                        }
+                    }
+
+                    System.out.println("You won! You made it to mars!");
+                    System.out.println("Crew Members left: " + crewAlive);
+                    System.out.println("Days taken: " + missionDay);
+
+                    running = false;
+                    break;
+                } else {
+                    // Assume everyone died
+                    boolean isEveryoneDead = true;
+
+                    // If someone's alive, stop!
+                    for (Crew member : crewMembers) {
+                        if (member.alive) {
+                            isEveryoneDead = false;
+                            break;
+                        }
+                    }
+
+                    if (flyingShip.oxygen <= 0 || flyingShip.fuel <= 0 || isEveryoneDead) {
+                        System.out.println("You lost :(");
+                        System.out.println("You couldn't make it to Mars, unfortunately.");
+                        System.out.println("Total Days: " + missionDay);
+                        System.out.println("Fuel left: " + flyingShip.fuel);
+                        System.out.println("Oxygen left: " + flyingShip.oxygen);
+                        System.out.println("Did everyone die? " + isEveryoneDead);
+
+                        running = false;
+                        break;
+                    }
+                }
+
                 // Run events and stuff if traveling
                 if (flyingShip.isTravelling) {
                     // Disable travel status and continue with journey
                     flyingShip.isTravelling = false;
                     missionDay += 1;
                     distance += rand.nextInt(6) + 5;
+                    event = "Clear";
 
                     resourceHistory.append("\nDAY ").append(missionDay).append(":");
 
@@ -79,14 +144,37 @@ public class Mission {
                     int eventChance = 50;
 
                     // Add 20% chance if not maintained
-                    if (!flyingShip.condition.equals("Maintained")) {
+                    if (!SHIP_CONDITIONS[flyingShip.condition].equals("Maintained")) {
                         eventChance += 20;
                     }
 
+                    // Choose event if chance is rolled
                     int rolledChance = rand.nextInt(99) + 1;
-                    if (rolledChance <= eventChance) {
-                        // TODO: choose weather
+                    if (rolledChance <= eventChance && event.equals("Clear")) {
+                        int chosenEvent = rand.nextInt(EVENTS.length);
+
+                        if (chosenEvent == 0) {
+                            // Meteor shower - worsen ship
+                            flyingShip.condition = Math.min(flyingShip.condition + 1, SHIP_CONDITIONS.length - 1);
+                        } else if (chosenEvent == 1) {
+                            // Solar Flare - harm crewmates
+                            for (Crew mate : crewMembers) {
+                                if (mate.alive) {
+                                    mate.health = Math.max(mate.health - 5, 5);
+                                    mate.crewStatus = Math.min(mate.crewStatus + 1, mate.STATUS_NAMES.length - 1);
+                                }
+                            }
+                        } else {
+                            // Supply Cache - give supplies
+                            flyingShip.supplies += 10;
+                        }
+
+                        // Set event to chosen event for information purposes
+                        event = EVENTS[chosenEvent];
                     }
+
+                    // Worsen ship condition
+                    flyingShip.condition = Math.min(flyingShip.condition + 1, SHIP_CONDITIONS.length - 1);
                 }
             }
 
@@ -99,7 +187,7 @@ public class Mission {
             // Action menu
             System.out.println("\nChoose an action:");
             System.out.println("1 - Travel (-FUEL/OXYGEN, +DISTANCE)");
-            System.out.println("2 - Maintain Ship (-SUPPLIES, +SHIP STATUS)");
+            System.out.println("2 - Maintain Ship (-SUPPLIES, +SHIP STATUS/FUEL)");
             System.out.println("3 - Ration (-CREW HEALTH)");
             System.out.println("4 - Rest (+CREW HEALTH, -SUPPLIES)");
             System.out.println("5 - View Resource History");
@@ -108,13 +196,15 @@ public class Mission {
             int choice = sc.nextInt();
 
             // Using switch instead of if-else because it's easier
-            int memberInt = 0;
+            int memberInt;
             switch (choice) {
                 case 1:
+                    // Attempt to travel
                     flyingShip.travel(crewMembers, resourceHistory);
 
                     break;
                 case 2:
+                    // Choose who to maintain ship and attempt it
                     System.out.println("Which crew member will maintain the ship?");
 
                     for (int i = 0; i < crewMembers.length; i++) {
@@ -126,10 +216,11 @@ public class Mission {
                     System.out.print("Choice: ");
 
                     memberInt = Math.clamp(sc.nextInt(), 0, crewMembers.length - 1);
-                    flyingShip.maintain(crewMembers[memberInt]);
+                    flyingShip.maintain(crewMembers[memberInt], resourceHistory);
 
                     break;
                 case 3:
+                    // Choose who is rationing for next day
                     System.out.println("Which crew member will be rationing?");
 
                     for (int i = 0; i < crewMembers.length; i++) {
@@ -137,14 +228,22 @@ public class Mission {
 
                         System.out.println(i + " - " + member.name + " / " + member.getAction());
                     }
+                    System.out.println(crewMembers.length + " - All members");
 
                     System.out.print("Choice: ");
+                    memberInt = Math.clamp(sc.nextInt(), 0, crewMembers.length);
 
-                    memberInt = Math.clamp(sc.nextInt(), 0, crewMembers.length - 1);
-                    flyingShip.ration(crewMembers[memberInt]);
+                    if (memberInt == crewMembers.length) {
+                        for (Crew member : crewMembers) {
+                            flyingShip.ration(member);
+                        }
+                    } else {
+                        flyingShip.ration(crewMembers[memberInt]);
+                    }
 
                     break;
                 case 4:
+                    // Choose who is resting for next day
                     System.out.println("Which crew member will be resting?");
 
                     for (int i = 0; i < crewMembers.length; i++) {
@@ -152,11 +251,18 @@ public class Mission {
 
                         System.out.println(i + " - " + member.name + " / " + member.getAction());
                     }
+                    System.out.println(crewMembers.length + " - All members");
 
                     System.out.print("Choice: ");
+                    memberInt = Math.clamp(sc.nextInt(), 0, crewMembers.length);
 
-                    memberInt = Math.clamp(sc.nextInt(), 0, crewMembers.length - 1);
-                    flyingShip.rest(crewMembers[memberInt]);
+                    if (memberInt == crewMembers.length) {
+                        for (Crew member : crewMembers) {
+                            flyingShip.rest(member);
+                        }
+                    } else {
+                        flyingShip.rest(crewMembers[memberInt]);
+                    }
 
                     break;
                 case 5:
@@ -164,6 +270,7 @@ public class Mission {
             }
         }
 
+        System.out.println("\nThanks for enjoying the mission!");
         sc.close();
     }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -4,83 +4,78 @@ public class Ship {
     int oxygen = 100;
     int supplies = 100;
 
-    String condition = "Maintained";
+    int condition = 0;
     boolean isTravelling = false;
 
     // Maintain the ship, costing a repair kit in exchange for less travel failure risk
-    public boolean maintain(Crew crewMember) {
+    public void maintain(Crew crewMember, StringBuilder resourceHistory) {
         // If already maintained, don't do anything
-        if (this.condition.equals("Maintained")) {
+        if (this.condition == 0) {
             System.out.println("The ship is already maintained! No worries.");
-            return false;
+            return;
         }
 
         // If we have no supplies we can't maintain the ship
         if (this.supplies < 15) {
             System.out.println("The ship ran out of supplies, so it cannot be maintained.");
-            return false;
+            return;
         }
 
         // Can't repair if you're resting, you need your sleep
         if (crewMember.isResting) {
             System.out.println(crewMember.name + " is resting and cannot maintain the ship!");
-            return false;
+            return;
         }
 
         // A dead person cannot maintain a ship
         if (!crewMember.alive) {
             System.out.println(crewMember.name + " is dead. They cannot maintain anything.");
-            return false;
+            return;
         }
 
         // Maintain the ship at the cost of supplies
+        this.fuel += 20;
         this.supplies -= 15;
-        this.condition = "Maintained";
+        this.condition = 0;
 
-        // Return that it was successful
-        return true;
+        resourceHistory.append("\n- 20 fuel was gained after ").append(crewMember.name).append(" maintained the ship.");
+        resourceHistory.append("\n- 15 supplies were consumed after ").append(crewMember.name).append(" maintained the ship.");
     }
 
     // Rest crew member, which will heal them and take resources next travel
-    public boolean rest(Crew crewMember) {
+    public void rest(Crew crewMember) {
         // Dead
         if (!crewMember.alive) {
             System.out.println(crewMember.name + " is permanently at rest. This won't help.");
-            return false;
+            return;
         }
 
         // Fail resting if there are no supplies to rest with
         if (this.supplies < 5) {
             System.out.println("The ship ran out of supplies, so " + crewMember.name + " cannot rest.");
-            return false;
+            return;
         }
 
         // Allow member to rest, removing rationing status.
         crewMember.isResting = true;
         crewMember.isRationing = false;
-
-        // Return that it was successful
-        return true;
     }
 
     // Ration supplies, which will damage them and save resources next travel
-    public boolean ration(Crew crewMember) {
+    public void ration(Crew crewMember) {
         // Dead, so cannot ration
         if (!crewMember.alive) {
             System.out.println(crewMember.name + " is dead. There's nothing to save.");
-            return false;
+            return;
         }
 
         // Allow member to ration supplies, removing Rest status
         crewMember.isResting = false;
         crewMember.isRationing = true;
-
-        // Return that it was successful
-        return true;
     }
 
     // Continue ship travel, adjusting status and is further used in Mission class
-    public boolean travel(Crew[] members, StringBuilder resourceHistory) {
+    public void travel(Crew[] members, StringBuilder resourceHistory) {
         // Check if members are resting or rationing
         boolean membersHaveActions = true;
         for (Crew member : members) {
@@ -93,7 +88,7 @@ public class Ship {
         // Fail to travel if members haven't been assigned an action
         if (!membersHaveActions) {
             System.out.println("All members have not been assigned an action!");
-            return false;
+            return;
         }
 
         // Update all crew members for next day & lose 5 oxygen per non rationing member
@@ -123,8 +118,5 @@ public class Ship {
         resourceHistory.append("\n- 10 fuel was consumed during travelling.");
         resourceHistory.append("\n- ").append(oxygenLoss).append(" oxygen was consumed during travelling.");
         resourceHistory.append("\n- ").append(supplyLoss).append(" supplies were consumed during travelling.");
-
-        // Return a successful travel
-        return true;
     }
 }
